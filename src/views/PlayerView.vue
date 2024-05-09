ac3a041d7e37538e746bd9d281d375500e298ca2
xuhui lin <xlin0541@outlook.com>
2024-05-09 23:50:42 +0100

    update

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -6,17 +6,27 @@
           <input v-model="latitude" placeholder="纬度" required />
           <input v-model="longitude" placeholder="经度" required />
           <br />
-          <button type="submit">提交</button>
+          <button type="submit">经纬度提交</button>
         </form>
         <form @submit.prevent="submitOffsets">
           <input v-model.number="offsetX" placeholder="偏移数值 X" required />
           <input v-model.number="offsetY" placeholder="偏移数值 Y" required />
           <input v-model.number="offsetZ" placeholder="偏移数值 Z" required />
           <br />
-          <button type="submit">提交</button>
+          <button type="submit">偏移数值提交</button>
         </form>
+        
       </div>
       <div class="data-display">
+        <form @submit.prevent="createLabel">
+          <input v-model.number="LabelX" placeholder="标签 X" required />
+          <input v-model.number="LabelY" placeholder="标签 Y" required />
+          <input v-model.number="LabelZ" placeholder="标签 Z" required />
+          <br />
+          <button type="submit">标签创建</button>
+        </form>
+        <button @click="toggleWhiteModel">{{ whiteModelCameraText }}</button>
+        <button @click="toggleTilesModel">{{ tilesModelCameraText }}</button>
         <table>
           <thead>
             <tr>
@@ -25,10 +35,8 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="item in items" :key="item.id">
-              <td>{{ item.id }}</td>
-              <td>{{ item.name }}</td>
-            </tr>
+            <td>{{ item.id}}</td>
+            <td>{{ item.name }}</td>
           </tbody>
         </table>
       </div>
@@ -36,51 +44,136 @@
   </template>
   
   <script>
-  import { Config, PixelStreaming } from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.3';
-  import { Application, PixelStreamingApplicationStyle } from '@epicgames-ps/lib-pixelstreamingfrontend-ui-ue5.3';
-  
-  export default {
-    name: 'PlayerView',
-    data() {
-      return {
-        toggle3DModelText: '开启3D模型',
-        is3DModelOn: false,
-        latitude: '',
-        longitude: '',
-        offsetX: 0,
-        offsetY: 0,
-        offsetZ: 0,
-        items: [],
-      };
-    },
-    mounted() {
-      const PixelStreamingApplicationStyles = new PixelStreamingApplicationStyle();
-      PixelStreamingApplicationStyles.applyStyleSheet();
-  
+import { Config, PixelStreaming } from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.3';
+import { Application, PixelStreamingApplicationStyle } from '@epicgames-ps/lib-pixelstreamingfrontend-ui-ue5.3';
+// import { is } from 'core-js/core/object';
+
+export default {
+  name: 'PlayerView',
+  data() {
+    return {
+      toggle3DModelText: '开启3D模型',
+      is3DModelOn: false,
+      isWhiteModelOn: false,
+      isTilesModelOn: false,
+      latitude: '',
+      longitude: '',
+      offsetX: 0,
+      offsetY: 0,
+      offsetZ: 0,
+      LabelX: 0,
+      LabelY: 0,
+      LabelZ: 0,
+      whiteModelCameraText: '白模摄像机位置',
+      tilesModelCameraText: '倾斜摄影摄像机位置',
+      item: { id: null, name: null },
+      stream: null,
+    };
+  },
+  mounted() {
+    this.initializePixelStreaming();
+    // const PixelStreamingApplicationStyles = new PixelStreamingApplicationStyle();
+    // PixelStreamingApplicationStyles.applyStyleSheet();
+
+    // const config = new Config({ useUrlParams: true });
+    // this.stream = new PixelStreaming(config); // 保存 stream 对象到 Vue 的数据属性中
+    // const application = new Application({
+    //   stream: this.stream,
+    //   onColorModeChanged: (isLightMode) => PixelStreamingApplicationStyles.setColorMode(isLightMode)
+    // });
+
+    // document.body.appendChild(application.rootElement);
+  },
+  methods: {
+    initializePixelStreaming() {
+    const PixelStreamingApplicationStyles = new PixelStreamingApplicationStyle();
+    PixelStreamingApplicationStyles.applyStyleSheet();
       const config = new Config({ useUrlParams: true });
-      const stream = new PixelStreaming(config);
+      this.stream = new PixelStreaming(config);
       const application = new Application({
-        stream,
-        onColorModeChanged: (isLightMode) => PixelStreamingApplicationStyles.setColorMode(isLightMode)
+        stream: this.stream,
+        onColorModeChanged: (isLightMode) => new PixelStreamingApplicationStyle().setColorMode(isLightMode)
       });
-  
       document.body.appendChild(application.rootElement);
+
+      // 假设 addResponseEventListener 是一个用于监听数据的方法
+      this.stream.addResponseEventListener('handle_responses', this.handleDataFromUE);
     },
-    methods: {
-      toggle3DModel() {
-        this.is3DModelOn = !this.is3DModelOn;
-        this.toggle3DModelText = this.is3DModelOn ? '关闭3D模型' : '开启3D模型';
-      },
-      submitLatLong() {
-        // Assume sending to the backend
-        console.log(`Submitted Latitude and Longitude: ${this.latitude}, ${this.longitude}`);
-      },
-      submitOffsets() {
-        // Assume sending to the backend
-        console.log(`Submitted Offsets: X=${this.offsetX}, Y=${this.offsetY}, Z=${this.offsetZ}`);
+
+    handleDataFromUE(data) {
+  console.log("Received data:", data);
+  try {
+    // 确认 data 是字符串并尝试解析它
+    const receivedData = typeof data === 'string' ? JSON.parse(data) : data;
+
+    // 更新 item 对象，仅更改接收到的字段
+    if (receivedData.id) {
+      this.item.id = receivedData.id;
+    }
+    if (receivedData.name) {
+      this.item.name = receivedData.name;
+    }
+
+    console.log("Updated item:", this.item);
+  } catch (error) {
+    console.error("Error parsing JSON:", error);
+  }
+},
+
+    toggle3DModel() {
+      this.is3DModelOn = !this.is3DModelOn;
+      this.toggle3DModelText = this.is3DModelOn ? '关闭3D模型' : '开启3D模型';
+      this.sendCommandToUE({ command: this.is3DModelOn ? 'open3DModel' : 'close3DModel' });
+    },
+
+    toggleWhiteModel() {
+      this.isWhiteModelOn = true;
+      // this.whiteModelCameraText = this.isWhiteModelOn ? '去白模' : '回原点';
+      this.sendCommandToUE({ command: this.isWhiteModelOn ? 'openWhiteModel' : 'closeWhiteModel' });
+    },
+
+    toggleTilesModel() {
+      this.isTilesModelOn = true;
+      // this.tilesModelCameraText = this.isTilesModelOn ? '去倾斜摄影' : '回原点';
+      this.sendCommandToUE({ command: this.isTilesModelOn ? 'openTiles' : 'closeTiles' });
+    },
+    
+    sendCommandToUE(descriptor) {
+      if (this.stream && typeof this.stream.emitUIInteraction === 'function') {
+        this.stream.emitUIInteraction(descriptor);
+      } else {
+        console.error('Stream not initialized or emitUIInteraction not available.');
       }
+    },
+
+    submitLatLong() {
+      const descriptor = {
+        command: 'updateLatLong',
+        latitude: this.latitude,
+        longitude: this.longitude
+      };
+      this.sendCommandToUE(descriptor);
+    },
+    submitOffsets() {
+      const descriptor = {
+        command: 'updateOffsets',
+        offsetX: this.offsetX,
+        offsetY: this.offsetY,
+        offsetZ: this.offsetZ
+      };
+      this.sendCommandToUE(descriptor);
+    },
+    createLabel() {
+      const descriptor = {
+        command: 'createLabel',
+        labelX: this.LabelX,
+        labelY: this.LabelY,
+        labelZ: this.LabelZ
+      };
+      this.sendCommandToUE(descriptor);
     }
   }
+}
   </script>
   
   <style>
@@ -96,7 +189,7 @@
     justify-content: space-between;
     align-items: flex-start; /* Adjust alignment */
     width: 100vw;
-    height: 25vh;
+    height: 20vh;
   }
   .controls {
     flex: 0 0 300px; /* No grow, no shrink, fixed width */
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true
 })
+module.exports = {
+    transpileDependencies: true,
+    devServer: {
+        port: 3000
+    }
+};
